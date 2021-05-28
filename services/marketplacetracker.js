6aa83d7b0d184b5f9e2d7ce3cc7fc00b39222245
jason <kwon_jason@yahoo.com>
2021-05-28 11:58:43 +1000

    update addr

--- services/marketplacetracker.js
+++ services/marketplacetracker.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose')
 
 const Listing = mongoose.model('Listing')
 const TradeHistory = mongoose.model('TradeHistory')
-const Account = mongoose.model('Account')
 const Offer = mongoose.model('Offer')
 const ERC721TOKEN = mongoose.model('ERC721TOKEN')
 const ERC1155TOKEN = mongoose.model('ERC1155TOKEN')
 const Category = mongoose.model('Category')
-
-const MailService = require('../utils/mailer')
 
 const contractUtils = require('../utils/contracts.utils')
 
@@ -37,17 +34,6 @@ const trackMarketPlace = () => {
       isPrivate,
       allowedAddress,
     ) => {
-      console.log('item listed')
-      console.log(
-        owner,
-        nft,
-        tokenID,
-        quantity,
-        pricePerItem,
-        startingTime,
-        isPrivate,
-        allowedAddress,
-      )
       owner = toLowerCase(owner)
       nft = toLowerCase(nft)
       allowedAddress = toLowerCase(allowedAddress)
@@ -102,13 +88,10 @@ const trackMarketPlace = () => {
 
   //   item sold
   marketplaceSC.on('ItemSold', async (seller, buyer, nft, tokenID, price) => {
-    console.log('item sold')
-
     seller = toLowerCase(seller)
     buyer = toLowerCase(buyer)
     nft = toLowerCase(nft)
     price = parseToFTM(price)
-    console.log(seller, buyer, nft, tokenID, price)
     // update last sale price
     // first update the token price
     let category = await Category.findOne({ minterAddress: nft })
@@ -132,6 +115,7 @@ const trackMarketPlace = () => {
           tokenID: tokenID,
         })
         if (token) {
+          token.price = parseFloat(price)
           token.lastSalePrice = parseFloat(price)
           token.soldAt = new Date() //set recently sold date
           token.listedAt = new Date(1970, 1, 1) //remove listed date
@@ -140,8 +124,6 @@ const trackMarketPlace = () => {
       }
     }
     try {
-      console.log('before save')
-      console.log(`seller is ${seller}`)
       // add new trade history
       let history = new TradeHistory()
       history.collectionAddress = nft
@@ -149,17 +131,13 @@ const trackMarketPlace = () => {
       history.to = buyer
       history.tokenID = tokenID
       history.price = price
-      let _hist = await history.save()
-      console.log('saved history')
-      console.log(_hist)
+      await history.save()
       // remove from listing
       let list = await Listing.findOne({
         owner: seller,
         minter: nft,
         tokenID: tokenID,
       })
-      console.log('found list is ')
-      console.log(list)
       if (parseInt(list.quantity) < 1) {
       } else if (parseInt(list.quantity) == 1) {
         await Listing.deleteOne({
@@ -170,15 +148,6 @@ const trackMarketPlace = () => {
       } else {
         list.quantity = parseInt(list.quantity) - 1
         await list.save()
-      }
-      // send an email notification to the seller
-      let sellerAccount = await Account.findOne({ address: seller })
-      if (sellerAccount) {
-        await MailService.sendEmail(
-          sellerAccount.email,
-          'Your item has been sold out!',
-          `{Dear ${sellerAccount.alias}, you are getting this email because your NFT item with id of ${tokenID} has been sold out at the price of ${price} by ${buyer}}`,
-        )
       }
     } catch (error) {
       console.log(error)
@@ -193,6 +162,31 @@ const trackMarketPlace = () => {
     owner = toLowerCase(owner)
     nft = toLowerCase(nft)
     price = parseToFTM(price)
+    // update the price of the nft here
+    // first update the token price
+    let category = await Category.findOne({ minterAddress: nft })
+    if (category) {
+      let type = parseInt(category.type)
+      if (type == 721) {
+        let token = await ERC721TOKEN.findOne({
+          contractAddress: nft,
+          tokenID: tokenID,
+        })
+        if (token) {
+          token.price = parseFloat(price)
+          await token.save()
+        }
+      } else if (type == 1155) {
+        let token = await ERC1155TOKEN.findOne({
+          contractAddress: nft,
+          tokenID: tokenID,
+        })
+        if (token) {
+          token.price = parseFloat(price)
+          await token.save()
+        }
+      }
+    }
   })
 
   //   item cancelled
@@ -259,16 +253,6 @@ const trackMarketPlace = () => {
       pricePerItem,
       deadline,
     ) => {
-      console.log('offer created')
-      console.log(
-        creator,
-        nft,
-        tokenID,
-        payToken,
-        quantity,
-        pricePerItem,
-        deadline,
-      )
       creator = toLowerCase(creator)
       nft = toLowerCase(nft)
       pricePerItem = parseToFTM(pricePerItem)
@@ -285,10 +269,8 @@ const trackMarketPlace = () => {
         offer.tokenID = tokenID
         offer.quantity = quantity
         offer.pricePerItem = pricePerItem
-        // offer.deadline = new Date(deadline)
         offer.deadline = deadline
         await offer.save()
-        // let account = Account.findOne({})
       } catch (error) {}
     },
   )
